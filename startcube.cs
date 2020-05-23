a1fa93421011f6f0688c0e5ba70cb4e0baff6449
The-Lamp <54476814+The-Lamp@users.noreply.github.com>
2020-05-23 21:34:08 +0200

    Add files via upload

---
+++ startcube.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startcube : MonoBehaviour
+{
+    public Rigidbody rigidbody3;
+    void Start()
+    {
+        Vector3 temp = new Vector3(5f, 10f, 0f);
+        gameObject.transform.position = temp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -9)
+        {
+            StartCoroutine("Drop");
+        }
+    }
+    IEnumerator Drop()
+    {
+        rigidbody3.velocity = Vector3.zero;
+        yield return new WaitForSeconds(3);
+        Vector3 temp = new Vector3(5f, 10f, 0f);
+        gameObject.transform.position = temp;
+    }
+}
